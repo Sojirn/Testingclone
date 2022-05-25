154de893b2c72030fcf52647bcea376a7d20b094
Sojirn <106176307+Sojirn@users.noreply.github.com>
2022-05-26 02:48:22 +0530

    New

---
+++ AddTwo.java
@@ -0,0 +1,13 @@
+class AddTwo
+{
+public static int add(int a,int b)
+{
+int c=a+b;
+return c;
+}
+public static void main(String args[])
+{
+int result=AddTwo.add(10,15);
+System.out.println("Sum is"+result);
+}
+}
